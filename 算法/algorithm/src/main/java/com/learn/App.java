5ec358251e7e77d482696ad72f74c3c9f1f6b37f
张志川 <zhangzhichuan@qianmi.com>
2021-03-27 17:58:29 +0800

    算法

---
+++ 算法/algorithm/src/main/java/com/learn/App.java
@@ -0,0 +1,16 @@
+package com.learn;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ * @author Administrator
+ */
+@SpringBootApplication
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
